8a6a5b3460a03339d0edcced8a754eab807cce9b
Stan Viltouski <vyts_vyts@mail.ru>
2018-11-17 16:48:06 +0300

    Slider Kamako

--- Камако/js/responsive.js
+++ Камако/js/responsive.js
@@ -7,8 +7,8 @@ function loaded() {
         timerID = null,
         pd = document.getElementById("css2"),
         dt = document.getElementById("css3"),
-        slide = document.getElementById("slideShow"),
-       pointF = document.querySelectorAll(".circles__cr");
+        slide = document.getElementById("slideShow");
+
 
     mb.disabled = pd.disabled = dt.disabled = true;
 
@@ -23,53 +23,18 @@ function loaded() {
         dt.disabled = true;
 
         if (window.innerWidth < 800) {
-
+            
             mb.disabled = false;
-            slide.style.left = "-320px";
-            startAutoSlide();
-          
+            slide.style.left="-320px";
 
         } else if ((window.innerWidth >= 800) && (window.innerWidth <= 1000)) {
-
+            
             pd.disabled = false;
-            slide.style.left = "-800px";
-            color("", "#fff", "");
-            startAutoSlide();
-
+            slide.style.left="-800px";
+        
         } else {
-
             dt.disabled = false;
-            slide.style.left = "-1358px";
-            color("", "#fff", "");
-            startAutoSlide();
-            
-            
-            /*
-            (function stopAutoSlide() {
-                if (timerID) {
-                    clearInterval(timerID);
-                    timerID = null;
-                }
-            }());
-            
-            */
-            
+           slide.style.left="-1358px"; 
         }
     }
-    
-     function color(val_1, val_2, val_3) {
-        pointF[0].style.backgroundColor = val_1;
-        pointF[1].style.backgroundColor = val_2;
-        pointF[2].style.backgroundColor = val_3;
-    }
-    
-        function startAutoSlide() {
-                if (!timerID) {
-                    timerID = setInterval(function () {
-
-                        slide.insertBefore(slide.lastElementChild, slide.firstElementChild);
-
-                    }, 12000);
-                }
-            };
 }
--- Камако/js/slider.js
+++ Камако/js/slider.js
@@ -1,94 +1,128 @@
 window.addEventListener("load", function () {
 
-
-    var buttonLeft = document.getElementById("arrows__arrL"),
-        buttonRight = document.getElementById("arrows__arrR"),
-        pointF = document.querySelectorAll(".circles__cr"),
-        slide = document.getElementById("slideShow");
     
-    
-    pointF[1].style.backgroundColor = "#fff";
+    funSlSh(document.querySelectorAll("#slideShow")[0], document.querySelectorAll(".circles__cr"));
 
+    function funSlSh(container, pointF) {
+        
+            pointF[1].style.backgroundColor='#fff';
+        var buttonLeft = document.getElementById("arrows__arrL"),
+            buttonRight = document.getElementById("arrows__arrR"),
+            timerID = null,
+            containerImg = container,
+            containerImgLeft = -1358,
+            containerImgStyle = containerImg.style,
+            windowWidth = 0,
+            countCircle = 1,
+            activeButton;
+            startAutoSlide();
+        
 
-    buttonLeft.addEventListener("click", initClick, false);
-    buttonRight.addEventListener("click", initClick, false);
-    pointF[0].addEventListener("click", initClick, false);
-    pointF[1].addEventListener("click", initClick, false);
-    pointF[2].addEventListener("click", initClick, false);
+          (function resize() {
+              if (window.innerWidth < 800) {windowWidth = -320;} 
+              else if ((window.innerWidth >= 800) && (window.innerWidth <= 1000)) {windowWidth = -800;} 
+              else {windowWidth = - 1358;}
+    }());
+      
 
-    function initClick(e) {
-        activeButton = e.target;
-        switch (e.target) {
+        container.addEventListener("mouseover", stopAutoSlide, false);
+        container.addEventListener("mouseout", startAutoSlide, false);
+        buttonLeft.addEventListener("mouseover", stopAutoSlide, false);
+        buttonLeft.addEventListener("mouseout", startAutoSlide, false);
+        buttonRight.addEventListener("mouseover", stopAutoSlide, false);
+        buttonRight.addEventListener("mouseout", startAutoSlide, false);
+        buttonLeft.addEventListener("click", initClick, false);
+        buttonRight.addEventListener("click", initClick, false);
 
-            case buttonLeft:
-                if (slide.style.left === "-1358px") {
-                    action(-2716);
-                    color("", "", "#fff");
-                    stopAutoSlide();
-                }
+        
+        function initClick(e) {
+            
+            activeButton = e.target;
+            if (activeButton !== buttonLeft && activeButton !== buttonRight) return;
+            
+            switch (e.type) {
+                case "click":
+                    if (containerImg.style.left === "-1358px") {
 
-                if (slide.style.left === "0px") {
-                    action(-1358);
-                    color("", "#fff", "");
-                }
+                        if (activeButton === buttonLeft) {
+                               containerImgLeft = -2716;
+                                ++countCircle;
+                               bkgCircle(); 
+                                                    
+                        } else {
+                              containerImgLeft = 0;
+                              --countCircle;
+                               bkgCircle();  
+                        }
 
-                break;
+                        TweenLite.to(containerImg, 0.5, {
+                            css: {
+                                left: containerImgLeft + "px"
+                            },
+                            onComplete: function () {transposition(containerImgLeft);
+                            }
+                        });
 
-            case buttonRight:
-                if (slide.style.left === "-2716px") {
-                    action(-1358);
-                    color("", "#fff", "");
-                }
-
-                if (slide.style.left === "-1358px") {
-                    action(0);
-                    color("#fff", "", "");
-                }
-
-                break;
-
-            case pointF[0]:
-                action(0);
-                color("#fff", "", "");
-                break;
-
-            case pointF[1]:
-                color("", "#fff", "");
-                if ((window.innerWidth >= 800) && (window.innerWidth <= 1000)) {
-             
-                    action(-800);
-                } else {
-                    action(-1358);
-                }
-
-                break;
-
-            case pointF[2]:
-                color("", "", "#fff");
-                if ((window.innerWidth >= 800) && (window.innerWidth <= 1000)) {
-
-                    action(-1600);
-                } else {
-                    action(-2716);
-                }
-                break;
-
-        }
-    }
-
-    function action(val) {
-        TweenLite.to(slide, 1, {
-            css: {
-                left: val + "px"
+                    }
+                    break;
             }
-        });
-    }
+            
+        }
 
-    function color(val_1, val_2, val_3) {
-        pointF[0].style.backgroundColor = val_1;
-        pointF[1].style.backgroundColor = val_2;
-        pointF[2].style.backgroundColor = val_3;
-    }
+        //переброс  блоков
+        
+        function transposition(d) {
+            if (d === 0) {
+                containerImg.insertBefore(containerImg.lastElementChild, containerImg.firstElementChild);
+                containerImg.style.left = windowWidth + "px";
 
+            } else if (d === (windowWidth*2)) {
+                containerImg.appendChild(containerImg.firstElementChild);
+                containerImg.style.left = windowWidth + "px";
+            }
+        }
+        
+        //цвет маркеров слайдера
+        
+            function bkgCircle() { 
+                pointF.forEach(function(Element){
+                    Element.style.backgroundColor="";
+                });
+                 if(countCircle>2) {countCircle=0} else if(countCircle<0) {countCircle=2}
+                pointF[countCircle].style.backgroundColor="#fff";
+              }
+        
+        //автослайдер
+        
+             function startAutoSlide() {
+                if (!timerID) {
+                    timerID = setInterval(function () {
+                        
+                          containerImgLeft = windowWidth*2;
+                        
+                          TweenLite.to(containerImg, 0.5, {
+                            css: {
+                                left: containerImgLeft + "px"
+                            },
+                            onComplete: function () {
+                                transposition(containerImgLeft);
+                            }
+                        });
+                    ++countCircle;
+                    bkgCircle(); 
+                        
+                     
+                    }, 12000);
+                }
+            };
+        
+        // стоп автослайдер
+          function stopAutoSlide() {
+                if (timerID) {
+                    clearInterval(timerID);
+                    timerID = null;
+                }
+            };
+    }
 
 }, false);
