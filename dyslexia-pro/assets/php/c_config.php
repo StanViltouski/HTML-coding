4632b8fbccd1ff3b95e5e6d7fa74e8abeb3baa27
StanViltouski <vyts_vyts@mail.ru>
2019-10-01 19:11:29 +0300

    active-pads(f)+dyslexia

---
+++ dyslexia-pro/assets/php/c_config.php
@@ -0,0 +1,7 @@
+<?php
+
+	define('XPEEDSTUDIO_EMAIL', 'overnoise@tut.by');
+	define('XPEEDSTUDIO_SUBJECT', 'New Contact Email');
+	define('XPEEDSTUDIO_SUCCESS_MASSAGE', 'Successfully Sent Email');
+
+?>
